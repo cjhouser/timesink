8d4b310f0876a6c16344190629d9d32275508064
Charles Houser <cjohouser@gmail.com>
2025-01-05 22:51:22 -0800

    unplanned resources can't use the subnet network
    Resources that are created without an explicit security group
    association is a mistake. Anything that gets created is put in the
    time out box.

--- infrastructure/atlantis/network.tf
+++ infrastructure/atlantis/network.tf
@@ -13,3 +13,11 @@ resource "aws_default_network_acl" "platform_default" {
     Name = "platform_default"
   }
 }
+
+resource "aws_default_security_group" "platform_default" {
+  vpc_id = aws_vpc.platform.id
+
+  tags = {
+    Name = "platform_default"
+  }
+}
