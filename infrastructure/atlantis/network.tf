76de82b868a9dc49034ba27864d9a31f9d0a1eda
Charles Houser <cjohouser@gmail.com>
2025-01-05 22:08:24 -0800

    unplanned subnets can't used the VPC
    NACLs are applied at the borders of subnets. Each subnet should define
    its own NACL depending on what is hosted in the subnet. Any subnet
    created without an explicit NACL gets put in a time out box.

--- infrastructure/atlantis/network.tf
+++ infrastructure/atlantis/network.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "platform" {
     Name       = "platform"
   }
 }
+
+resource "aws_default_network_acl" "platform_default" {
+  default_network_acl_id = aws_vpc.platform.default_network_acl_id
+
+  tags = {
+    Name = "platform_default"
+  }
+}
