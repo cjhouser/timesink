e1019509d253e7125a5197e6e365da684770fb55
Charles Houser <cjohouser@gmail.com>
2025-01-07 20:19:16 -0800

    two private subnets for ECS cluster
    Create public/private subnet pairs in accordance with thoughtlyify.io's
    subnetting strategy.
    
    Note that the aws_availability_zone data sources may not be compatible
    with other regions since the letter suffix are hardcoded.

---
+++ infrastructure/atlantis/data.tf
@@ -0,0 +1,9 @@
+data "aws_region" "current" {}
+
+data "aws_availability_zone" "a" {
+  name = "${data.aws_region.current.name}a"
+}
+
+data "aws_availability_zone" "c" {
+  name = "${data.aws_region.current.name}c"
+}
--- infrastructure/atlantis/network.tf
+++ infrastructure/atlantis/network.tf
@@ -9,6 +9,12 @@ resource "aws_vpc" "platform" {
 resource "aws_default_network_acl" "platform_default" {
   default_network_acl_id = aws_vpc.platform.default_network_acl_id
 
+  subnet_ids = [
+    aws_subnet.platform_private_a.id,
+    aws_subnet.platform_public_a.id,
+    aws_subnet.platform_private_c.id,
+    aws_subnet.platform_public_c.id
+  ]
   tags = {
     Name = "platform_default"
   }
@@ -28,5 +34,41 @@ resource "aws_default_route_table" "platform_default" {
 
   tags = {
     Name = "platform_default"
+  }
+}
+
+resource "aws_subnet" "platform_public_a" {
+  vpc_id            = aws_vpc.platform.id
+  availability_zone = data.aws_availability_zone.a.name
+  cidr_block        = "10.0.0.0/24"
+  tags = {
+    Name       = "platform_public_a"
+  }
+}
+
+resource "aws_subnet" "platform_private_a" {
+  vpc_id            = aws_vpc.platform.id
+  availability_zone = data.aws_availability_zone.a.name
+  cidr_block        = "10.0.4.0/24"
+  tags = {
+    Name       = "platform_private_a"
+  }
+}
+
+resource "aws_subnet" "platform_public_c" {
+  vpc_id            = aws_vpc.platform.id
+  availability_zone = data.aws_availability_zone.c.name
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name       = "platform_public_c"
+  }
+}
+
+resource "aws_subnet" "platform_private_c" {
+  vpc_id            = aws_vpc.platform.id
+  availability_zone = data.aws_availability_zone.c.name
+  cidr_block        = "10.0.5.0/24"
+  tags = {
+    Name       = "platform_private_c"
   }
 }
