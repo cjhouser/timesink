de079504b3e10712e78cf71d3f651f49d1adb623
Charles Houser <cjohouser@gmail.com>
2025-01-08 19:49:30 -0800

    atlantis task definition

--- infrastructure/atlantis/atlantis.tf
+++ infrastructure/atlantis/atlantis.tf
@@ -10,3 +10,37 @@ resource "aws_ecs_cluster" "platform" {
     }
   }
 }
+
+resource "aws_ecs_task_definition" "atlantis" {
+  family                   = "atlantis"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  container_definitions    = jsonencode([
+    {
+      name: "atlantis"
+      image: "ghcr.io/runatlantis/atlantis:dev-alpine-1c4f688"
+      command: [
+        "server",
+        "--gh-user=fake",
+        "--gh-token=fake",
+        "--repo-allowlist 'github.com/cjhouser/thoughtlyify.io'",
+        "--atlantis-url https://$ATLANTIS_HOST"
+      ]
+      cpu: 256
+      memory: 512
+      essential: true
+      portMappings = [
+        {
+          containerPort = 4141
+        }
+      ]
+    }
+  ])
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+}
